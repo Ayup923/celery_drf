5267882e4e2ac68bc176c11aa556d766fae61aef
Ayup923 <aupmaan@gmail.com>
2023-01-09 10:35:25 +0600

    celery-drf

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.142.216.255','127.0.0.1']
 
 
 # Application definition
